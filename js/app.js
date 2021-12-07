a10b9a24859f9f50c0e71a41ee8c8f111b8bfff1
Pranay <pranayguptamain@gmail.com>
2021-12-07 21:27:56 +0530

    feat: implement basic pen-up pen-down function

---
+++ js/app.js
@@ -0,0 +1,51 @@
+const canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth - 60;
+canvas.height = 400;
+
+let context = canvas.getContext("2d");
+context.fillStyle = "white";
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+let draw_color = "black";
+let draw_width = "2";
+let is_drawing = false;
+
+// for mobile and tablet
+canvas.addEventListener("touchstart", start, false);
+canvas.addEventListener("touchmove", draw, false);
+canvas.addEventListener("touchend", stop, false);
+
+// for computer mouse
+canvas.addEventListener("mousedown", start, false);
+canvas.addEventListener("mousemove", draw, false);
+canvas.addEventListener("mouseup", stop, false);
+canvas.addEventListener("mouseout", stop, false);
+
+function start(event) {
+    is_drawing = true;
+    context.beginPath();
+    context.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+
+    event.preventDefault();
+}
+
+function draw(event) {
+    if (is_drawing) {
+        context.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+        context.strokeStyle = draw_color;
+        context.lineWidth = draw_width;
+        context.lineCap = "round";
+        context.lineJoin = "round";
+        context.stroke();
+    }
+    event.preventDefault();
+}
+
+function stop(event) {
+    if (is_drawing) {
+        context.stroke();
+        context.closePath();
+        is_drawing = false;
+    }
+    event.preventDefault();
+}
